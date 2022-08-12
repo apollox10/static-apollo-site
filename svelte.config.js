308057924725a527ba4dc0432b5d052bf79b57e5
Julio Morales <julmoralesb@gmail.com>
2022-08-12 08:05:06 -0500

    Added firebase config

--- svelte.config.js
+++ svelte.config.js
@@ -1,20 +1,28 @@
-import adapter from '@sveltejs/adapter-auto';
-import preprocessor from "svelte-preprocess"
+import adapter from '@sveltejs/adapter-static';
+
+import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	
-	preprocess: preprocessor({
-		scss: {
-			prependData: "@import './static/style.scss';"
-		}
-	}),
+    // Consult https://github.com/sveltejs/svelte-preprocess
+    // for more information about preprocessors
+    preprocess: [
+        preprocess({
+          postcss: true,
+        }),
+      ],
 
-	
-	kit: {
-		adapter: adapter()
-	}
+    kit: {
+        adapter: adapter({
+            pages: 'public',
+            assets: 'public',
+            fallback: null,
+            precompress: false
+            }),
+            prerender: {
+            default: true
+            }
+    }
 };
-
 
 export default config;
