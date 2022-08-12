9a59dcd5e80982e1984355878331c156d8c3d276
Julio Morales <julmoralesb@gmail.com>
2022-08-12 08:50:09 -0500

    Trying again

--- svelte.config.js
+++ svelte.config.js
@@ -1,26 +1,19 @@
 import adapter from '@sveltejs/adapter-static';
 
-import preprocess from 'svelte-preprocess';
+export default {
+  kit: {
+    adapter: adapter({
+      // default options are shown. On some platforms
+      // these options are set automatically — see below
+      pages: 'build',
+      assets: 'build',
+      fallback: null,
+      precompress: false
+    }),
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-    preprocess: [
-        preprocess({
-          postcss: true,
-        }),
-      ],
-
-    kit: {
-        adapter: adapter({
-            pages: 'public',
-            assets: 'public',
-            fallback: null,
-            precompress: false
-            }),
-            prerender: {
-            default: true
-            }
+    prerender: {
+      // This can be false if you're using a fallback (i.e. SPA mode)
+      default: true
     }
+  }
 };
-
-export default config;
