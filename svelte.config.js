7dc239f92112ac10bb9eae7e98daae807e360aa1
Julio Morales <julmoralesb@gmail.com>
2022-08-22 07:17:15 -0500

    Changed homepage to TypeScript

--- svelte.config.js
+++ svelte.config.js
@@ -1,19 +1,25 @@
 import adapter from '@sveltejs/adapter-static';
+import preprocess from 'svelte-preprocess';
 
-export default {
-  kit: {
-    adapter: adapter({
-      // default options are shown. On some platforms
-      // these options are set automatically — see below
-      pages: 'build',
-      assets: 'build',
-      fallback: null,
-      precompress: false
-    }),
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	// Consult https://github.com/sveltejs/svelte-preprocess
+	// for more information about preprocessors
+	preprocess: preprocess(),
 
-    prerender: {
-      // This can be false if you're using a fallback (i.e. SPA mode)
-      default: true
-    }
-  }
+	kit: {
+		adapter: adapter({
+			// default options are shown. On some platforms
+			// these options are set automatically — see below
+			pages: 'build',
+			assets: 'build',
+			precompress: false
+		}),
+		prerender: {
+			// This can be false if you're using a fallback (i.e. SPA mode)
+			default: true
+		}
+	}
 };
+
+export default config;
